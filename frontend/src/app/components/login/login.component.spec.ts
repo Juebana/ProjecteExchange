ebbceb07e636f7c49e934e7813ee765bc5840194
Guanlin Wang <guanlin987@gmail.com>
2025-01-21 18:35:55 +0100

    implemented the test that should show the inputs and the send button of the login

--- frontend/src/app/components/login/login.component.spec.ts
+++ frontend/src/app/components/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,7 +8,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent, FormsModule],
     })
     .compileComponents();
 
@@ -19,5 +19,18 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería mostrar los inputs para username y password, y el botón de envío', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const usernameInput = compiled.querySelector('input#username');
+    const passwordInput = compiled.querySelector('input#password');
+    const submitButton = compiled.querySelector('button[type="submit"]');
+
+    expect(usernameInput).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+    expect(submitButton).toBeTruthy();
+    expect(submitButton?.textContent?.toLowerCase()).toContain('inicia sessió');
   });
 });
