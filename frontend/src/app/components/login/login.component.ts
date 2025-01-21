2233821672c7520d7dfbc2c256d244ee98be2263
Guanlin Wang <guanlin987@gmail.com>
2025-01-21 19:01:38 +0100

    implemented the ts and updated the html to pass the previous test

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  get isFormValid(): boolean {
+    return this.username.trim() !== '' && this.password.trim() !== '';
+  }
 }
