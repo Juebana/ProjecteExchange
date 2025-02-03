1b3738304fd097231d00632431d5fb750ae7a1bc
Guanlin Wang <guanlin987@gmail.com>
2025-02-03 17:30:18 +0100

    implemented the ngoninit on the ts to get the username from localstorage

--- frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -10,13 +10,17 @@ import { Router } from '@angular/router';
 })
 export class NavBarComponent {
   constructor(private router: Router) {}
-  
+  username: string | null = null;
   logout(): void {
     console.log('Logout clicked');
   }
 
   goToProfile(): void {
     this.router.navigate(['/profile']);
+  }
+
+  ngOnInit(): void {
+    this.username = localStorage.getItem('username');
   }
 }
 
