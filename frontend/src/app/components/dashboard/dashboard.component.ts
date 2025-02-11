4e9a43395902d1240408511f115c3e2686d45755
Guanlin Wang <guanlin987@gmail.com>
2025-02-11 19:55:01 +0100

    updated the dashboard html to contain the chart component, passing the test

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { ChartComponent } from "../chart/chart.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, ChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
