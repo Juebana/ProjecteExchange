7e901061cc341f758f980f85a9793fca8d46edcb
Guanlin Wang <guanlin987@gmail.com>
2025-02-11 19:43:31 +0100

    implemented the tradingview chart code to use their chart on the project

--- frontend/src/app/components/chart/chart.component.ts
+++ frontend/src/app/components/chart/chart.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [],
   templateUrl: './chart.component.html',
-  styleUrl: './chart.component.css'
+  styleUrls: ['./chart.component.css']
 })
-export class ChartComponent {
+export class ChartComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    const script = document.createElement('script');
+    script.src = 'https://s3.tradingview.com/tv.js';
+    script.async = true;
+    script.onload = () => this.initializeChart();
+    this.elementRef.nativeElement.appendChild(script);
+  }
+
+  private initializeChart(): void {
+    new (window as any).TradingView.widget({
+      container_id: 'tradingview_chart',
+      width: '100%',
+      height: '100%',
+      symbol: 'BINANCE:PEPEUSDC',
+      interval: '1',
+      timezone: 'Etc/UTC',
+      theme: 'dark',
+      style: '1',
+      locale: 'en',
+      enable_publishing: false,
+      hide_top_toolbar: true,
+      hide_legend: true,
+      withdateranges: false
+    });
+  }
 }
