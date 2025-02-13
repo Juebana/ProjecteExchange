79d243546b2cf3f52b4c686307cb9cb3c58b2a79
Guanlin Wang <guanlin987@gmail.com>
2025-02-13 19:39:04 +0100

    implemented some constants on the buys sell ts, and also the openTrade function

--- frontend/src/app/components/buysell/buysell.component.ts
+++ frontend/src/app/components/buysell/buysell.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './buysell.component.html',
   styleUrl: './buysell.component.css'
 })
-export class BuysellComponent {
+export class BuySellComponent {
+  isBuy: boolean = true;
+  isLimit: boolean = false;
+  price: number | null = null;
+  amount: number | null = null;
 
+  openTrade(): void {
+    console.log(`Placing a ${this.isBuy ? 'BUY' : 'SELL'} order. Type: ${this.isLimit ? 'Limit' : 'Market'}, Price: ${this.price}, Amount: ${this.amount}`);
+  }
 }
+
