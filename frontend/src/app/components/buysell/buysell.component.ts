3799043ab082fa2692a868377d8877f46d9a1599
Guanlin Wang <guanlin987@gmail.com>
2025-02-13 18:58:39 +0100

    generated the buysell component

---
+++ frontend/src/app/components/buysell/buysell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buysell',
+  standalone: true,
+  imports: [],
+  templateUrl: './buysell.component.html',
+  styleUrl: './buysell.component.css'
+})
+export class BuysellComponent {
+
+}
