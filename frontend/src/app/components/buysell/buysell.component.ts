1f98d23ef7cc36693d85609daa8a1b8467e11763
Guanlin Wang <guanlin987@gmail.com>
2025-02-18 18:52:09 +0100

    updated the buysell ts to hold some more variables

--- frontend/src/app/components/buysell/buysell.component.ts
+++ frontend/src/app/components/buysell/buysell.component.ts
@@ -7,16 +7,23 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './buysell.component.html',
-  styleUrl: './buysell.component.css'
+  styleUrls: ['./buysell.component.css']
 })
 export class BuySellComponent {
   isBuy: boolean = true;
   isLimit: boolean = false;
   price: number | null = null;
   amount: number | null = null;
+  selectedCurrency: string = 'usdc';
+  acceptedTerms: boolean = false; 
 
   openTrade(): void {
-    console.log(`Placing a ${this.isBuy ? 'BUY' : 'SELL'} order. Type: ${this.isLimit ? 'Limit' : 'Market'}, Price: ${this.price}, Amount: ${this.amount}`);
+    console.log(
+      `Placing a ${this.isBuy ? 'BUY' : 'SELL'} order. 
+       Type: ${this.isLimit ? 'Limit' : 'Market'}, 
+       Price: ${this.price}, 
+       Amount: ${this.amount} ${this.selectedCurrency}, 
+       Terms accepted: ${this.acceptedTerms}`
+    );
   }
 }
-
