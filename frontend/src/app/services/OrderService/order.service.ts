2db4ae74078b13037cab44f1a7aeda72826896a6
Guanlin Wang <guanlin987@gmail.com>
2025-02-20 19:28:54 +0100

    implemented an easy order service

--- frontend/src/app/services/OrderService/order.service.ts
+++ frontend/src/app/services/OrderService/order.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Order } from '../../models/order.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  private orderUrl = 'http://localhost:3000/orders';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createOrder(orderData: Partial<Order>): Observable<Order> {
+    return this.http.post<Order>(this.orderUrl, orderData);
+  }
 }
