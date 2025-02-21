5e4d6c0d5f138502061cdc1df06d80aa8291228b
Guanlin Wang <guanlin987@gmail.com>
2025-02-21 19:44:47 +0100

    updated the order service to convert the order object to a plain object before sending it to the backend

--- frontend/src/app/services/OrderService/order.service.ts
+++ frontend/src/app/services/OrderService/order.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Order } from '../../models/order.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  private orderUrl = 'http://localhost:3000/orders';
+  private orderUrl = 'http://localhost:3000/order/postOrder';
 
   constructor(private http: HttpClient) {}
 
-  createOrder(orderData: any): Observable<Order> {
-    return this.http.post<any>(this.orderUrl, orderData).pipe(
-      map(response => new Order(
-        response.id,
-        response.userId,
-        response.tradeSide,
-        response.tradeType,
-        response.price,
-        response.amount,
-        response.currency,
-        response.created_at
-      ))
-    );
+  createOrder(order: Order): Observable<any> {
+    const orderData = {
+      userId: order.userId,
+      tradeSide: order.tradeSide,
+      tradeType: order.tradeType,
+      price: order.price,
+      amount: order.amount,
+      currency: order.currency
+    };
+    return this.http.post<any>(this.orderUrl, orderData);
   }
 }
