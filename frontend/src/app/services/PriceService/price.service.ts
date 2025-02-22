7de59ee6da46e6a7311a7c664fb500d77fdb0970
Guanlin Wang <guanlin987@gmail.com>
2025-02-22 18:09:38 +0100

    implemented the price service to do a get request to to a binance api

--- frontend/src/app/services/PriceService/price.service.ts
+++ frontend/src/app/services/PriceService/price.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PriceService {
+  private binanceApiUrl = 'https://api.binance.com/api/v3/ticker/price';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCurrentPrice(symbol: string): Observable<number> {
+    return this.http.get<{ symbol: string; price: string }>(`${this.binanceApiUrl}?symbol=${symbol}`).pipe(
+      map(response => parseFloat(response.price))
+    );
+  }
 }
