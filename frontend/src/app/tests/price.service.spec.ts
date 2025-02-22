f6ea82cd495c536789a75dd249dbc2c2bc798c94
Guanlin Wang <guanlin987@gmail.com>
2025-02-22 18:08:51 +0100

    implemented the provider httpclient on the price service ts to solve the nullinjector error

--- frontend/src/app/tests/price.service.spec.ts
+++ frontend/src/app/tests/price.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PriceService } from './price.service';
+import { PriceService } from '../services/PriceService/price.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PriceService', () => {
   let service: PriceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()]
+    });
     service = TestBed.inject(PriceService);
   });
 
