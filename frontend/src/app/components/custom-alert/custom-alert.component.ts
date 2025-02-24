2fa75486ea3d45837e9d7ff70f765ab561b2e00c
Guanlin Wang <guanlin987@gmail.com>
2025-02-24 20:54:17 +0100

    implemented the custom alert html, and updated some imports from its ts

--- frontend/src/app/components/custom-alert/custom-alert.component.ts
+++ frontend/src/app/components/custom-alert/custom-alert.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-alert',
   templateUrl: './custom-alert.component.html',
   styleUrls: ['./custom-alert.component.css'],
-  standalone: true 
+  standalone: true,
+  imports: [FormsModule,CommonModule]
 })
 export class CustomAlertComponent {
   @Input() showAlert: boolean = false;
