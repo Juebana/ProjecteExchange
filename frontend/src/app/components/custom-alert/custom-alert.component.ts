febd99217f36655c374357debf16ce8268d87363
Guanlin Wang <guanlin987@gmail.com>
2025-02-24 20:43:02 +0100

    generated the custom alert component

---
+++ frontend/src/app/components/custom-alert/custom-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-alert.component.html',
+  styleUrl: './custom-alert.component.css'
+})
+export class CustomAlertComponent {
+
+}
