4b3c44366c6d5c598b353bdb9dfd6830ca7381ad
Guanlin Wang <guanlin987@gmail.com>
2025-02-24 18:33:20 +0100

    updated the app routes to export the routes also

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
