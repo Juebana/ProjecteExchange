2e26268f22714bf7d89ce88fec5f08b5c069857f
Guanlin Wang <guanlin987@gmail.com>
2025-02-24 17:17:03 +0100

    implemented the api url on the enviroment.ts

---
+++ frontend/src/enviroments/enviroment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    loginUrl: 'http://localhost:3000/auth/login',
+    registerUrl: 'http://localhost:3000/auth/register',
+    orderUrl: 'http://localhost:3000/order/postOrder',
+    priceUrl: 'https://api.binance.com/api/v3/ticker/price'
+};
