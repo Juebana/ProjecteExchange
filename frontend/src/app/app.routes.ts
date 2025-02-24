3658ecf10ab63cbe1d49d450041c5b7e33ba6c3b
Guanlin Wang <guanlin987@gmail.com>
2025-02-24 18:31:24 +0100

    updated the app routes to include the register route too

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,18 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'login', component: LoginComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: '**', redirectTo: 'login' }
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
