9471513bd54cdb50d2a2b4588d046ac57e74d253
Guanlin Wang <guanlin987@gmail.com>
2025-02-26 23:54:29 +0100

    implemented the subtract funds method on the fundservice

--- frontend/src/app/services/FundService/fund.service.ts
+++ frontend/src/app/services/FundService/fund.service.ts
@@ -7,9 +7,9 @@ import { environment } from '../../../enviroments/enviroment';
   providedIn: 'root'
 })
 export class FundService {
-  private apiUrl = environment.fundUrl; 
+  private apiUrl = environment.fundUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getBalance(userId: string): Observable<{ balance: number }> {
     const headers = new HttpHeaders({ 'user-id': userId });
@@ -19,5 +19,10 @@ export class FundService {
   rechargeFunds(userId: string, amount: number): Observable<{ message: string, newBalance: number }> {
     const headers = new HttpHeaders({ 'user-id': userId });
     return this.http.post<{ message: string, newBalance: number }>(`${this.apiUrl}/recharge`, { amount }, { headers });
+  }
+
+  subtractFunds(userId: string, amount: number): Observable<{ message: string, newBalance: number }> {
+    const headers = new HttpHeaders({ 'user-id': userId });
+    return this.http.post<{ message: string, newBalance: number }>(`${this.apiUrl}/subtract`, { amount }, { headers });
   }
 }
