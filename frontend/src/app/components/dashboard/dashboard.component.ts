e89d8e2e88070dcd1581afa60ab71ffd43e4f36a
Guanlin Wang <guanlin987@gmail.com>
2025-02-27 18:40:11 +0100

    implementented the order list on the dashboard

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ChartComponent } from "../chart/chart.component";
 import { BuySellComponent } from '../buysell/buysell.component';
+import { OrderListComponent } from '../order-list/order-list.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavBarComponent, ChartComponent, BuySellComponent],
+  imports: [NavBarComponent, ChartComponent, BuySellComponent, OrderListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
