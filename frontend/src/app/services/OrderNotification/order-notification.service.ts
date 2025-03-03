bfdbddf2105920e09abfa848dce47d05c9a6868b
Guanlin Wang <guanlin987@gmail.com>
2025-03-03 21:34:21 +0100

    implemented the notify order placed function on the order notification service

--- frontend/src/app/services/OrderNotification/order-notification.service.ts
+++ frontend/src/app/services/OrderNotification/order-notification.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderNotificationService {
+  private orderPlacedSource = new Subject<void>();
+  orderPlaced$ = this.orderPlacedSource.asObservable();
 
-  constructor() { }
+  notifyOrderPlaced() {
+    this.orderPlacedSource.next();
+  }
 }
